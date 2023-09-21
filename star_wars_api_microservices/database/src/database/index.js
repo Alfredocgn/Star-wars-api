a3f272ffa9137adfa2b0a148e10ed56e7a655384
Alfredocgn <alfredocgn@gmail.com>
2023-09-20 23:27:21 -0300

    planets added

--- star_wars_api_microservices/database/src/database/index.js
+++ star_wars_api_microservices/database/src/database/index.js
@@ -4,3 +4,7 @@ const {MONGO_URI}= require('../config/envs')
 const conn = mongoose.createConnection(MONGO_URI)
 
 const Character = conn.model("Character",'schemas/characterSchema.js')
+const Film = conn.model("Film",'schemas/filmSchema.js')
+const Planet = conn.model("Planet",'schemas/planetSchema.js')
+
+Character.find().populate('films').then((res)=> console.log(res))
